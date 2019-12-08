1ba9b3e9824150df1ffcb0c89859d2962ece3c75
Hubert Siwkin <hsiwkin@gmail.com>
2019-12-08 01:10:03 +0100

    Handle creating floor boxes

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject floorBox;
+    public int size = 10;
+
+    private void Awake()
+    {
+        Arrange();
+    }
+
+    private void Arrange()
+    {
+        for (int x = 0; x < size; ++x)
+        {
+            for (int z = 0; z < size; ++z)
+            {
+                CreateFloorBox(x, z);
+            }
+        }
+    }
+
+    private void CreateFloorBox(float positionX, float positionZ)
+    {
+        float boxSize = floorBox.transform.localScale.x;
+        var boxInstance = Instantiate(
+            floorBox,
+            new Vector3(boxSize * positionX, 0, boxSize * positionZ),
+            Quaternion.identity
+        );
+        var boxInstanceRenderer = boxInstance.GetComponent<Renderer>();
+        var material = boxInstanceRenderer.material;
+
+        float hue, saturation, value;
+        Color.RGBToHSV(material.color, out hue, out saturation, out value);
+
+        material.color = Random.ColorHSV(hue, hue, saturation, saturation, 0.85f, 1f);
+    }
+}
