27fe9bac981618f6a61b25ff8d2478b3a4d7434e
Hubert Siwkin <hsiwkin@gmail.com>
2019-12-13 19:58:35 +0100

    Add animator variables & moving mocks

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,8 @@ public class GameManager : MonoBehaviour
     public GameObject floorBox, crate, wall, targetFloorBox, player;
     public Transform mainCamera;
     private GameState gameState;
+
+    private bool performingAction = false;
     
 
     private void Awake()
@@ -27,7 +29,12 @@ public class GameManager : MonoBehaviour
 
         SetupCamera();
     }
-    
+
+    private void Update()
+    {
+        HandleInput();
+    }
+
     private void SetupCamera()
     {
         mainCamera.transform.position = new Vector3(
@@ -35,5 +42,34 @@ public class GameManager : MonoBehaviour
             mainCamera.transform.position.y,
             gameState.MapSize[0] / 2
         );
+    }
+
+    private void HandleInput()
+    {
+        // TODO: it's a mock - implement it
+
+        var verticalInput = Input.GetAxis("Vertical");
+        var horizontalInput = Input.GetAxis("Horizontal");
+
+        if (Mathf.Abs(horizontalInput) > 0)
+        {
+            MoveRight();
+        }
+    }
+
+    private void MoveRight()
+    {
+        performingAction = true;
+        var position = gameState.playerPosition;
+        var width = position[1];
+        var height = position[0];
+
+        Debug.Log("target width: " + (width + 1));
+        Debug.Log("target height: " + height);
+
+
+        
+
+        performingAction = false;
     }
 }
